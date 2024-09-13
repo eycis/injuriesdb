2ba8c0427d1a1968a775ea5f3598588f6343386f
eycis <dzugam00@jcu.cz>
2024-09-13 23:12:04 +0200

    Head button update

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,7 +6,7 @@ const Head = () => {
     <div className="w-[100%] fixed z-[10000] top-0 h-[10vh] bg-[#141c27] shadow-md print:hidden">
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
           <Link to='/'>
-            <button className="flex-[0.6] uppercase cursor-pointer text-[25px] text-white font-bold hover:text-blue-700 transition duration-300">
+            <button className="flex-[0.6] uppercase cursor-pointer text-[25px] text-white font-bold hover:text-blue-400 transition duration-300">
               Elektronická Kniha <span className="text-blue-400">Úrazů</span>
             </button>
           </Link>
